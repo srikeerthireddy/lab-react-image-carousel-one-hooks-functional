2930a7b5772296bbccf0a27cbdf4afb58540c6fb
srikeerthireddy <srikeerthireddy24@gmail.com>
2024-01-29 16:30:13 +0530

    Image-Carousel-Functioanl-components

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,53 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
-function Carousel() {
-}
+const Carousel = () => {
+  const [currImageIndex, setCurrImageIndex] = useState(0);
+
+  const prev = () => {
+    if (currImageIndex > 0) {
+      setCurrImageIndex(currImageIndex - 1);
+    } else {
+      setCurrImageIndex(images.length - 1);
+    }
+  };
+
+  const next = () => {
+    if (currImageIndex < images.length - 1) {
+      setCurrImageIndex(currImageIndex + 1);
+    } else {
+      setCurrImageIndex(0);
+    }
+  };
+
+  return (
+    <>
+      <div className="main-container">
+        <div className="btn-container">
+          <div className="left-btn" onClick={prev}>
+            <ArrowBackIosIcon style={{ fill: "white" }} />
+          </div>
+
+          <div className="right-btn" onClick={next}>
+            <ArrowForwardIosIcon style={{ fill: "white" }} />
+          </div>
+        </div>
+
+        <div className="image-span-container">
+          <span className="text title">
+            {images[currImageIndex].title}
+          </span>
+          <img src={images[currImageIndex].img} alt="" />
+          <span className="text subtitle">
+            {images[currImageIndex].subtitle}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Carousel;
